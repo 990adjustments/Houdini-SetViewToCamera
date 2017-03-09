f537b1b4bbde4d5512a8b0f74b79b54c8a855bcf
Erwin Santacruz <escruz@gmail.com>
2017-03-09 00:44:09 -0500

    Inital Commit

---
+++ SetViewToCamera.md
@@ -0,0 +1,9 @@
+= Set View to Camera =
+
+*version 1.0*
+
+"""Saves the current viewport state to the selected camera.
+----
+http://houdinitricks.com
+@houdiniTricks
+"""
---
+++ setViewToCamera.py
@@ -0,0 +1,23 @@
+"""Set the current view to the selected camera.
+
+Usage:
+
+First Select a camera in your scene.
+"""
+
+def saveViewportToCam(camera_node):
+    desktop = hou.ui.curDesktop()
+    scene_viewer = desktop.paneTabOfType(hou.paneTabType.SceneViewer)
+    viewport = scene_viewer.curViewport()
+    
+    # set camera
+    viewport.saveViewToCamera(camera_node)
+
+try:
+    # camera selection
+    cam = hou.selectedNodes()[0]
+    saveViewportToCam(cam)
+    setStatusMessage("Camera view set", severity=hou.severityType.Message)
+    
+except IndexError:
+    hou.ui.displayMessage("Please select a camera.", severity=hou.severityType.Error, title="No camera selected")
